1f23b4f027b281bb1f21e465c669077fdb0a4fb3
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-09-02 11:01:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-class BoardMember {
-
-constructor(name, homeState, training){
+function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-}
 
   this.prototype.veto = function(){
     return 'No, I must disagree'
@@ -23,5 +20,4 @@ constructor(name, homeState, training){
   this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-
 }
